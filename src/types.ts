4dea58f4ffd70d8851ce9e1fbe00c5ac381ce9bf
aldarg <dargeev@gmail.com>
2021-01-20 19:58:40 +0700

    fix TS-error with Request + userId

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,12 @@ import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core';
 import { Response, Request } from 'express';
 import { Session, SessionData } from 'express-session';
 
+declare module 'express-session' {
+  interface Session {
+    userId: number;
+  }
+}
+
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
   req: Request & {
